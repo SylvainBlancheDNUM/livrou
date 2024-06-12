e1e4e2bcb549af9abdde1ed3feb372a62918412c
Sylvain Blanche <blanche_sylvain@yahoo.fr>
2024-06-12 09:26:34 +0200

    fix: clean menu

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -48,9 +48,7 @@ export default function NavigationMenu() {
         <section className="relative mx-auto">
             <nav className="flex justify-between bg-gray-900 text-white w-screen">
                 <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-                    <a className="text-3xl font-bold font-heading" href="#">
-                        Logo Here.
-                    </a>
+
                     <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
                         <li>
                             <Link href={"/"}>
